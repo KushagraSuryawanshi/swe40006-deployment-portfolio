1566a5d33dd59f12763589aee8ee87428d818aa5
kushagra suryawanshi <kushagrasuryavanshi123@gmail.com>
2026-08-17 23:58:36 +1000

    feat: complete credit-level custom app deployment

---
+++ Portfolio Task 1/Task1/DeploymentChecker/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace DeploymentChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Deployment folder path:");
+            string? userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput) || !Directory.Exists(userInput)) {
+                Console.WriteLine("Directory does not exist");
+            }
+            else
+            {
+                Console.WriteLine("Directory found");
+                string[] allFiles = Directory.GetFiles(userInput);
+                long totalSize = 0;
+
+                Console.WriteLine($"File count: {allFiles.Length}");
+
+                bool hasExe = false;
+                bool hasDll = false;
+                bool hasJson = false;
+
+                foreach (string file in allFiles)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    totalSize += fileInfo.Length;
+
+                    Console.WriteLine(Path.GetFileName(file));
+
+                    string extension = Path.GetExtension(file);
+
+                    if (extension == ".exe") 
+                    {
+                        hasExe = true;
+                    }
+                    if (extension == ".dll") 
+                    {
+                        hasDll = true;
+                    }
+                    if (extension == ".json")
+                    {
+                        hasJson = true;
+                    }
+                
+                }
+
+                Console.WriteLine($"Total size: {totalSize} bytes");
+                Console.WriteLine($"EXE found: {hasExe}");
+                Console.WriteLine($"DLL found: {hasDll}");
+                Console.WriteLine($"JSON found: {hasJson}");
+
+                if (hasExe && hasDll && hasJson)
+                {
+                    Console.WriteLine("Deployment package looks valid.");
+                }
+                else
+                {
+                    Console.WriteLine("Deployment package looks incomplete.");
+                }
+            }
+
+           
+        }
+    }
+}
